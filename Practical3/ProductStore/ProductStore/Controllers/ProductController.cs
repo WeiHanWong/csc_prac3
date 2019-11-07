1cf0cffba9ebcbbe36f52ae2cf3cab21bfb2d681
WeiHanWong <56218253+WeiHanWong@users.noreply.github.com>
2019-11-08 02:08:02 +0800

    first

---
+++ Practical3/ProductStore/ProductStore/Controllers/ProductController.cs
@@ -0,0 +1,86 @@
+﻿using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductStore.Controllers
+{
+    public class ProductController : ApiController
+    {
+        static readonly IProductRepository repository = new ProductRepository();
+
+
+        //Version 3
+        //[Authorize]
+        [HttpGet]
+        [Route("api/v3/products")]
+        public IEnumerable<Product> GetAllProductsFromRepository()
+        {
+            return repository.GetAll();
+
+        }
+
+        [HttpGet]
+        [Route("api/v3/products/{id:int:min(2)}", Name = "getProductByIdv3")]
+
+        public Product retrieveProductfromRepository(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return item;
+        }
+
+
+        [HttpGet]
+        [Route("api/v3/products", Name = "getProductByCategoryv3")]
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return repository.GetAll().Where(
+                p => string.Equals(p.Category, category, StringComparison.OrdinalIgnoreCase));
+        }
+
+        [HttpPost]
+        [Route("api/v3/products")]
+        public HttpResponseMessage PostProduct(Product item)
+        {
+            if (ModelState.IsValid)
+            {
+                item = repository.Add(item);
+                var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item);
+
+                string uri = Url.Link("getProductByIdv3", new { id = item.Id });
+                response.Headers.Location = new Uri(uri);
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        [HttpPut]
+        [Route("api/v3/products/{id:int}")]
+        public void PutProduct(int id, Product product)
+        {
+            product.Id = id;
+            if (!repository.Update(product))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/v3/products/{id:int}")]
+        public void DeleteProduct(int id)
+        {
+            repository.Remove(id);
+        }
+
+    }
+}
